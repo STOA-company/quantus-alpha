ac13d11622a5972ccb7d5d94bbb14a6066c42e3e
kyungmin-k <rhrudals312@mement.ai>
2024-11-14 18:59:31 +0900

    Chore: swagger 설정

--- app/main.py
+++ app/main.py
@@ -2,7 +2,11 @@ from fastapi import FastAPI
 from app.core.config import settings
 from app.api import routers
 
-app = FastAPI(title=settings.PROJECT_NAME)
+app = FastAPI(
+    title=settings.PROJECT_NAME,
+    description="Alphafinder API Documentation",
+    version="1.0.0",
+)
 app.include_router(routers.router)
 
 
