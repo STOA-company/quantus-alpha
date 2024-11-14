0f5549c538405f9c184a5337ff92762c2fdfedb4
kyungmin-k <rhrudals312@mement.ai>
2024-11-14 21:30:06 +0900

    pre commit 및 세팅 (#3)
    * Chore: pre-commit 세팅
    
    * Chore: 이슈, Pr 템플릿 생성
    
    * Rename: router 파일 구조 수정
    
    * Chore: swagger 설정
    
    * Chore: sqlalchemy, pymysql, python-dotenv 추가
    
    * Feat: db 연결
    
    * Feat: CRUD class 작성
    
    * Fix: on_event -> asynccontextmanager 수정

--- app/main.py
+++ app/main.py
@@ -1,6 +1,8 @@
-from fastapi import FastAPI
-from app.core.config import settings
+from fastapi import Depends, FastAPI
+from requests import Session
+from app.core.config import get_database_config, settings
 from app.api import routers
+from app.database.conn import db
 
 app = FastAPI(
     title=settings.PROJECT_NAME,
@@ -9,7 +11,21 @@ app = FastAPI(
 )
 app.include_router(routers.router)
 
+db_config = get_database_config()
+db.init_app(app, **db_config.__dict__)
+
+
 
 @app.get("/")
 def root():
     return {"message": "Welcome to the Financial Data API !!"}
+
+
+@app.get("/health-check")
+async def health_check(session: Session = Depends(db.get_db)):
+    try:
+        session.execute("SELECT 1")
+        return {"status": "healthy", "database": "connected"}
+    except Exception as e:
+        return {"status": "unhealthy", "database": str(e)}
+
