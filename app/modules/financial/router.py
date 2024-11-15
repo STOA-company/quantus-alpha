7521fad48f3e057137de748d1d9c91b475108dab
donggeun <kdg3729@mement.ai>
2024-11-15 14:45:20 +0900

    Feat : finpos api 구현

--- app/modules/financial/router.py
+++ app/modules/financial/router.py
@@ -1,7 +1,7 @@
 from app.modules.common.enum import Country
 from fastapi import APIRouter, Depends
 from app.modules.financial.services import FinancialService, get_financial_service
-from .schemas import CashFlowResponse, FinancialDataResponse, IncomeStatementResponse
+from .schemas import CashFlowResponse, FinPosResponse, FinancialDataResponse, IncomeStatementResponse
 
 router = APIRouter()
 
@@ -41,7 +41,7 @@ router = APIRouter()
 #     df = await service.read_financial_data("cashflow", ctry, ticker)
 #     return {"data": df.to_dict(orient="records")}
 
-@router.get("/income", response_model=IncomeStatementResponse, summary="재무제표 분기별 조회")
+@router.get("/income", response_model=IncomeStatementResponse, summary="손익계산서 분기별 조회")
 async def get_income_data(
     ctry: Country,
     ticker: str,
@@ -60,6 +60,18 @@ async def get_cashflow_data(
     financial_service: FinancialService = Depends(get_financial_service),
 ) -> CashFlowResponse:
     result = await financial_service.get_cashflow_data(
+        ctry=ctry,
+        ticker=ticker
+    )
+    return result
+
+@router.get("/finpos", response_model=FinPosResponse, summary="재무제표 분기별 조회")
+async def get_finpos_data(
+    ctry: Country,
+    ticker: str,
+    financial_service: FinancialService = Depends(get_financial_service),
+) -> FinPosResponse:
+    result = await financial_service.get_finpos_data(
         ctry=ctry,
         ticker=ticker
     )
