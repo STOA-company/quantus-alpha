02024e8c7e2b66b363723ec900289b4a3555050e
kyungmin-k <rhrudals312@mement.ai>
2024-11-19 15:36:46 +0900

    FIX: us 선택시 안나오는 에러 수정 (#19)

--- app/modules/common/enum.py
+++ app/modules/common/enum.py
@@ -4,3 +4,17 @@ from enum import Enum
 class Country(Enum):
     KR = "kr"
     US = "us"
+
+
+class Frequency(Enum):
+    DAILY = "daily"
+    MINUTE = "minute"
+
+
+class GraphPeriod(Enum):
+    ONE_DAY = "oneday"
+    ONE_WEEK = "oneweek"
+    ONE_MONTH = "onemonth"
+    THREE_MONTH = "threemonth"
+    ONE_YEAR = "oneyear"
+    ALL = "all"
