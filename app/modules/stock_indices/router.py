f4f3fcd4a6313b0c81af6d9b1aca9c864020e893
donggeun <kdg3729@mement.ai>
2024-12-05 13:52:12 +0900

    Feat : 지수 전용 api 생성

---
+++ app/modules/stock_indices/router.py
@@ -0,0 +1,28 @@
+from fastapi import APIRouter, Depends
+from .service import StockIndicesService
+
+router = APIRouter()
+
+
+@router.get("/five", summary="코스피/코스닥 지수 조회 5분 간격")
+async def get_stock_indices_five(
+    service: StockIndicesService = Depends(StockIndicesService),
+):
+    result = await service.get_indices_data()
+
+    if not result:
+        return {"status_code": 404, "message": "오늘의 거래 데이터가 아직 없습니다.", "data": None}
+
+    return {"status_code": 200, "message": "코스피/코스닥 지수를 성공적으로 조회했습니다.", "data": result}
+
+
+@router.get("/fifteen", summary="코스피/코스닥 지수 조회 15분 간격")
+async def get_stock_indices_fifteen(
+    service: StockIndicesService = Depends(StockIndicesService),
+):
+    result = await service.get_indices_data_fifteen()
+
+    if not result:
+        return {"status_code": 404, "message": "오늘의 거래 데이터가 아직 없습니다.", "data": None}
+
+    return {"status_code": 200, "message": "코스피/코스닥 지수를 성공적으로 조회했습니다.", "data": result}
