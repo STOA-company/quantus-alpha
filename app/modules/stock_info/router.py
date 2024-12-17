916db6fa69ec43501960e32ec9a37105fd5a3af5
donggeun <kdg3729@mement.ai>
2024-12-17 14:04:05 +0900

    Feat : 지표 api 구현

--- app/modules/stock_info/router.py
+++ app/modules/stock_info/router.py
@@ -2,7 +2,7 @@ from fastapi import APIRouter, Depends
 
 from app.modules.common.enum import Country
 from app.modules.common.schemas import BaseResponse
-from app.modules.stock_info.schemas import StockInfo
+from app.modules.stock_info.schemas import Indicators, StockInfo
 from .services import StockInfoService, get_stock_info_service
 
 router = APIRouter()
@@ -10,9 +10,19 @@ router = APIRouter()
 
 @router.get("", response_model=BaseResponse[StockInfo], summary="주식 정보 조회")
 async def get_stock_info(
-    ticker: str,
     ctry: Country,
+    ticker: str,
     service: StockInfoService = Depends(get_stock_info_service),
 ):
-    data = await service.get_stock_info(ticker, ctry)
+    data = await service.get_stock_info(ctry, ticker)
     return BaseResponse(status_code=200, message="주식 정보를 성공적으로 조회했습니다.", data=data)
+
+
+@router.get("/indicators", response_model=BaseResponse[Indicators], summary="기업 정보 조회")
+async def get_indicators(
+    ctry: Country,
+    ticker: str,
+    service: StockInfoService = Depends(get_stock_info_service),
+):
+    data = await service.get_indicators(ctry, ticker)
+    return BaseResponse(status_code=200, message="기업 정보를 성공적으로 조회했습니다.", data=data)
