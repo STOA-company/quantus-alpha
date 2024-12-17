62e16853dc82cf803079e1c3925bcea73226a21f
kyungmin-k <rhrudals312@mement.ai>
2024-12-17 21:17:43 +0900

    Feat: 배당금 api

--- app/api/v1.py
+++ app/api/v1.py
@@ -5,6 +5,7 @@ from app.modules.stock_indices.router import router as stock_indices_router
 from app.modules.news.router import router as news_router
 from app.modules.stock_info.router import router as stock_info_router
 from app.modules.disclosure.router import router as disclosure_router
+from app.modules.dividend.router import router as dividend_router
 
 api_router = APIRouter()
 
@@ -14,3 +15,4 @@ api_router.include_router(stock_indices_router, prefix="/stock-indices", tags=["
 api_router.include_router(news_router, prefix="/news", tags=["news"])
 api_router.include_router(stock_info_router, prefix="/info", tags=["stock-info"])
 api_router.include_router(disclosure_router, prefix="/disclosure", tags=["disclosure"])
+api_router.include_router(dividend_router, prefix="/dividend", tags=["dividend"])
