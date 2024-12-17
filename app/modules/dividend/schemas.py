dd7c9c8955672928344b46dfec8a2ab7c41e6914
donggeun <kdg3729@mement.ai>
2024-12-17 21:45:22 +0900

    Fix : 배당 Mock 데이터

--- app/modules/dividend/schemas.py
+++ app/modules/dividend/schemas.py
@@ -3,11 +3,15 @@ from pydantic import BaseModel, Field
 
 
 class DividendDetail(BaseModel):
-    year: int = Field(description="연도")
     ex_dividend_date: str = Field(description="배당락일")
     dividend_payment_date: str = Field(description="배당지급일")
     dividend_per_share: float = Field(description="1주당 배당금")
     dividend_yield: float = Field(description="수익률")
+
+
+class DividendYearResponse(BaseModel):
+    year: int = Field(description="연도")
+    dividend_detail: List[DividendDetail] = Field(description="연도별 배당 상세 정보")
 
 
 class DividendItem(BaseModel):
@@ -15,4 +19,4 @@ class DividendItem(BaseModel):
     last_dividend_per_share: float = Field(description="직전 1주당 배당금")
     last_dividend_ratio: float = Field(description="직전 1주당 배당비율")
     last_dividend_growth_rate: float = Field(description="직전 배당 성장률")
-    detail: List[DividendDetail] = Field(description="배당 상세 정보")
+    detail: List[DividendYearResponse] = Field(description="배당 상세 정보")
