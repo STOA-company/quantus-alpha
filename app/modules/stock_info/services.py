f6a627031a239cb21316644a1fbeac3d82d54a79
kyungmin-k <rhrudals312@mement.ai>
2024-12-17 21:32:34 +0900

    Feat: response 수정 ( 기업 소개 )

--- app/modules/stock_info/services.py
+++ app/modules/stock_info/services.py
@@ -22,13 +22,18 @@ class StockInfoService:
             raise DataNotFoundException(ticker=ctry.name, data_type="stock_info")
 
         file_name = self.file_name.format(ctry.name)
-        FILE_PATH = f"{self.file_path}/{file_name}"
-        df = pd.read_csv(FILE_PATH)
+        info_file_path = f"{self.file_path}/{file_name}"
+        df = pd.read_csv(info_file_path)
         result = df.loc[df["ticker"] == ticker].to_dict(orient="records")[0]
         if result is None:
             raise DataNotFoundException(ticker=ticker, data_type="stock_info")
 
+        intro_file_path = f"{self.file_path}/summary_{ctry.name}.parquet"
+        intro_df = pd.read_parquet(intro_file_path)
+        intro_result = intro_df.loc[intro_df["Code"] == ticker].to_dict(orient="records")[0]
+
         result = StockInfo(
+            introduction=intro_result.get("translated_overview", ""),
             homepage_url=result["URL"],
             ceo_name=result["LastName"] + result["FirstName"],
             establishment_date=result["IncInDt"],
