6348250b3d47f87ccf990eddb064a0f26d383b96
kyungmin-k <rhrudals312@mement.ai>
2024-12-17 21:33:28 +0900

    Feat: response 수정 ( 기업 소개 ) (#72)

--- app/modules/stock_info/schemas.py
+++ app/modules/stock_info/schemas.py
@@ -3,6 +3,7 @@ from pydantic import BaseModel, Field
 
 
 class StockInfo(BaseModel):
+    introduction: str | None = Field(description="주식 소개")
     homepage_url: str | None = Field(description="홈페이지 주소")
     ceo_name: str | None = Field(description="대표자 이름")
     establishment_date: str | None = Field(description="설립일")
