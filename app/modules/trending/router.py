48fb4f7e26485a910ac6f0a087014010b6543b83
kyungmin-k <rhrudals312@mement.ai>
2024-12-20 16:51:11 +0900

    Fix : 급상승 종목 로직 수정

--- app/modules/trending/router.py
+++ app/modules/trending/router.py
@@ -1,6 +1,4 @@
 from fastapi import APIRouter, Depends
-from sqlalchemy.ext.asyncio import AsyncSession
-from app.database.conn import db
 from app.modules.trending.schemas import TrendingStock
 from app.modules.trending.service import TrendingService, get_trending_service
 
@@ -9,8 +7,7 @@ router = APIRouter()
 
 
 @router.get("", summary="급상승 종목 조회")
-async def get_trending_stocks(
+def get_trending_stocks(
     service: TrendingService = Depends(get_trending_service),
-    db: AsyncSession = Depends(db.get_async_db),
 ) -> TrendingStock:
-    return await service.get_trending_stocks()
+    return service.get_trending_stocks()
