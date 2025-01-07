0166675cd1cf00065a8401f37a8675cda21c9d61
kyungmin-k <rhrudals312@mement.ai>
2025-01-07 21:35:37 +0900

    Revert "Feat : celey 세팅 (#211)" (#212)
    This reverts commit 008900c6e1e2f382250974a7df12538578f11b62.

--- app/core/config.py
+++ app/core/config.py
@@ -17,7 +17,7 @@ class Settings(BaseSettings):
     # App settings
     ENV: str = ENV
     DEBUG: bool = os.getenv("DEBUG", "False").lower() == "true"
-    PROJECT_NAME: str = f"Alphafinder API 1.0 - {ENV}"
+    PROJECT_NAME: str = "Alphafinder API 1.0"
     API_V1_STR: str = "/api/v1"
     API_V2_STR: str = "/api/v2"
     DATA_DIR: str = os.getenv("DATA_DIR", "./data")
@@ -69,15 +69,6 @@ class DevConfig(DatabaseConfig):
         )
 
 
-class StageConfig(DatabaseConfig):
-    def __init__(self):
-        super().__init__(
-            DB_URL=f"mysql://{settings.RDS_USER}:{settings.RDS_PASSWORD}@{settings.RDS_HOST}:{settings.RDS_PORT}/{settings.RDS_DB}",
-            DB_POOL_RECYCLE=3600,
-            DB_ECHO=True,
-        )
-
-
 class ProdConfig(DatabaseConfig):
     def __init__(self):
         super().__init__(
@@ -99,5 +90,5 @@ class TestConfig(DatabaseConfig):
 def get_database_config():
     """Get database configuration based on environment"""
 
-    config = dict(prod=ProdConfig, dev=DevConfig, test=TestConfig, stage=StageConfig)
+    config = dict(prod=ProdConfig, dev=DevConfig, test=TestConfig)
     return config[settings.ENV]()
