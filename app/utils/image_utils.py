d57275e6514d724471e6b18f313a79dcf17a3b3d
김광윤 <ivk@gimgwang-yuns-MacBook-Air.local>
2025-02-17 14:16:25 +0900

    hotfix: 파일 처리 동기적으로 수정

--- app/utils/image_utils.py
+++ app/utils/image_utils.py
@@ -6,8 +6,8 @@ def convert_file_to_base64(file: UploadFile) -> str:
     """
     FastAPI의 UploadFile을 base64 문자열로 변환
     """
-    contents = file.read()
+    contents = file.file.read()
     base64_encoded = base64.b64encode(contents).decode("utf-8")
-    file.seek(0)
+    file.file.seek(0)  # 파일 포인터를 처음으로
 
     return base64_encoded
