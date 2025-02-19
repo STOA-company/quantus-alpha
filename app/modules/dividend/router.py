7907e992cbd1628b8978c20015e93be2674b09c2
kyungmin-stoa <rhrudals312@stoa-investment.com>
2025-02-19 15:58:05 +0900

    Fix/div 배당 api수정 (#368)
    * 배당 데이터 수정
    
    * Fix : 배당 모델 수정
    
    * Fix : 배당 모델 수정(pk 수정)
    
    * Fix : 모델 수정
    
    * Fix : dividend 수정

--- app/modules/dividend/router.py
+++ app/modules/dividend/router.py
@@ -10,11 +10,21 @@ from app.modules.dividend.services import DividendService, get_dividend_service
 router = APIRouter()
 
 
-@router.get("", response_model=BaseResponse[DividendItem], summary="배당 정보 조회")
+@router.get("/old", response_model=BaseResponse[DividendItem], summary="배당 정보 조회")
 async def get_dividend(
     ticker: Annotated[str, Query(description="종목 코드", min_length=1)],
     service: DividendService = Depends(get_dividend_service),
 ) -> BaseResponse[DividendItem]:
     ctry = check_ticker_country_len_2(ticker)
     dividend_data = await service.get_dividend(ctry=ctry, ticker=ticker)
+    return BaseResponse(status_code=200, message="배당 정보를 성공적으로 조회했습니다.", data=dividend_data)
+
+
+@router.get("", response_model=BaseResponse[DividendItem], summary="배당 정보 조회")
+async def get_dividend_renewal(
+    ticker: Annotated[str, Query(description="종목 코드", min_length=1)],
+    service: DividendService = Depends(get_dividend_service),
+) -> BaseResponse[DividendItem]:
+    ctry = check_ticker_country_len_2(ticker)
+    dividend_data = await service.get_dividend_renewal(ctry=ctry, ticker=ticker)
     return BaseResponse(status_code=200, message="배당 정보를 성공적으로 조회했습니다.", data=dividend_data)
