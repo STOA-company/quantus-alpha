ade2ccb62362d3b903f80b97bc82ad1b5311f5b3
kyungmin-stoa <rhrudals312@stoa-investment.com>
2025-02-19 14:22:02 +0900

    Fix : 배당 모델 수정

--- app/models/models_dividend.py
+++ app/models/models_dividend.py
@@ -1,18 +1,22 @@
 from pydantic import model_validator
-from sqlalchemy import Column, Date, Float, PrimaryKeyConstraint, String
+from sqlalchemy import Column, Date, Float, Integer, String, UniqueConstraint, Index
 from app.models.models_base import Base
 
 
 class Dividend(Base):
     __tablename__ = "dividend_information"
 
+    id = Column(Integer, primary_key=True, autoincrement=True)
     ticker = Column(String(20), nullable=False, comment="종목 티커")  #
     payment_date = Column(Date, nullable=False, comment="배당 지급일")  #
     ex_date = Column(Date, nullable=False, comment="배당 락일")  #
     per_share = Column(Float, nullable=True, comment="1주당 배당금")  #
     yield_rate = Column(Float, nullable=True, comment="배당 수익률")  # 배당금 / 종가
 
-    __table_args__ = (PrimaryKeyConstraint("ticker", "ex_date"),)
+    __table_args__ = (
+        UniqueConstraint("ticker", "ex_date", "payment_date", name="uix_ticker_ex_date_payment_date"),
+        Index("idx_ticker_ex_date", "ticker", "ex_date"),
+    )
 
     @model_validator(mode="after")
     def validate_ex_date(self) -> "Dividend":
