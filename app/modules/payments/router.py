fc86e84a28546e11fde894fd205ff075be85de15
김광윤 <ivk@gimgwang-yuns-MacBook-Air.local>
2025-03-19 17:59:55 +0900

    hotfix: timedelta 수정

--- app/modules/payments/router.py
+++ app/modules/payments/router.py
@@ -17,12 +17,13 @@ def confirm_toss_payments(
         toss_payment_receipt.payment_key, toss_payment_receipt.order_id, toss_payment_receipt.amount, current_user.id
     )
 
-    price = toss_payment_receipt.amount
-    if price == 19000:
-        timedelta = 30
-    elif price == 190000:
-        timedelta = 365
+    amount = toss_payment_receipt.amount
+    period = 0
+    if amount == 19000:
+        period = 30
+    elif amount == 190000:
+        period = 365
     else:
         raise HTTPException(status_code=400, detail="Invalid price")
-    payment_service.user_subscription_update(current_user.id, timedelta)
+    payment_service.user_subscription_update(current_user.id, period)
     return {"status": "success"}
