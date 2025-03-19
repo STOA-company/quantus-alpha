d009a2cadd6fba1c878845949296519a88aa1f0a
김광윤 <ivk@gimgwang-yuns-MacBook-Air.local>
2025-03-19 17:58:06 +0900

    hotfix: 파라미터 수정

--- app/modules/payments/router.py
+++ app/modules/payments/router.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
 from app.modules.payments.service import PaymentService
 from app.modules.payments.schema import TossPaymentReceipt
 from app.models.models_users import AlphafinderUser
@@ -7,14 +7,22 @@ from app.utils.oauth_utils import get_current_user
 router = APIRouter()
 
 
-@router.post("/toss")
-def get_toss_payments_receipt(
+@router.post("/toss/confirm")
+def confirm_toss_payments(
     toss_payment_receipt: TossPaymentReceipt,
     current_user: AlphafinderUser = Depends(get_current_user),
     payment_service: PaymentService = Depends(PaymentService),
 ):
     payment_service.store_toss_payments_history(
-        toss_payment_receipt.payment_key, toss_payment_receipt.order_id, toss_payment_receipt.price, current_user.id
+        toss_payment_receipt.payment_key, toss_payment_receipt.order_id, toss_payment_receipt.amount, current_user.id
     )
-    payment_service.user_subscription_update(current_user.id, toss_payment_receipt.timedelta)
+
+    price = toss_payment_receipt.amount
+    if price == 19000:
+        timedelta = 30
+    elif price == 190000:
+        timedelta = 365
+    else:
+        raise HTTPException(status_code=400, detail="Invalid price")
+    payment_service.user_subscription_update(current_user.id, timedelta)
     return {"status": "success"}
--- app/modules/payments/schema.py
+++ app/modules/payments/schema.py
@@ -4,5 +4,4 @@ from pydantic import BaseModel
 class TossPaymentReceipt(BaseModel):
     payment_key: str
     order_id: str
-    timedelta: int
-    price: int
+    amount: int
