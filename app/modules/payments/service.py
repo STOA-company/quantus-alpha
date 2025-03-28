8b0565dd6d881059cf223ffe829ad499d905631d
kyungmin-stoa <rhrudals312@stoa-investment.com>
2025-03-28 19:06:24 +0900

    Fix/payments (#545)
    * Feat : 모델 추가 (level, price, payment history, toss receipt
    
    * fix : 1차 payment
    
    * Fix : 모델 수정
    
    * fix: 모델 수정
    
    * Fix : 사용자 level 추가
    
    * Fix : 모델 수정
    
    * Fix : 결제 승인 api
    
    * fix: 변수 수정
    
    * Fix : 결제 로직 예외 처리
    
    * Fix : 쿠폰 정보 확인, 쿠폰 등록 api 생성
    
    * fix : 상태 변경
    
    * Fix : 모델 수정(coupon.expried_at 추가)
    
    * Fix : 쿠폰 등록, 쿠폰 사용 api
    
    * Fix : 멤버십 관리 화면 사용권 확인 수정
    
    * Fix : api 에러 수정
    
    * Fix : 멤버십 api 수정
    
    * fix : /me response 수정
    
    * chore : 주석 제거
    
    * Fix : price_template 수정
    
    * fix : schema type 변경
    
    * Fix : price template 롤백
    
    * fix : price template 롤 백
    
    * Fix : /me res 수정
    
    * Fix : membership api 수정
    
    * Fix : 에러 수정
    
    * fix : membership 수정
    
    * fix : payment
    
    * fix :

--- app/modules/payments/service.py
+++ app/modules/payments/service.py
@@ -229,20 +229,26 @@ class PaymentService:
     ):
         data = self.db._select(
             table="alphafinder_user",
-            columns=["is_subscribed", "subscription_end"],
+            columns=["is_subscribed", "subscription_end", "subscription_start", "using_history_id"],
             id=user_id,
         )
         if not data:
             raise Exception(f"유저 정보 조회 실패: {user_id}")
         is_subscribed = data[0].is_subscribed
         subscription_end = data[0].subscription_end
-
+        subscription_start = data[0].subscription_start
+        using_history_id = data[0].using_history_id
         if is_subscribed:
             update_user_subscription = UpdateUserSubscription(
+                is_subscribed=True,
+                subscription_start=subscription_start,
                 subscription_end=subscription_end + timedelta(days=(period + 1)),
                 recent_payment_date=now_kr().date(),
+                subscription_level=level,
+                subscription_name=product_name,
+                using_history_id=using_history_id,
             )
-            self.update_is_extended(user_using_history_id)
+            self.update_is_extended(using_history_id)
         else:
             update_user_subscription = UpdateUserSubscription(
                 is_subscribed=True,
