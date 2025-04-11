41ddb733101928c4aea1f3b5da401b4e895bb92f
leorivk <112841516+leorivk@users.noreply.github.com>
2025-04-11 17:41:56 +0900

    feat: URL rewrite (#663)
    Co-authored-by: 김광윤 <ivk@gimgwang-yuns-MacBook-Air.local>

--- scripts/service-deploy.sh
+++ scripts/service-deploy.sh
@@ -135,8 +135,8 @@ server {
     }
 
     location /grafana/ {
-        rewrite  ^/grafana/(.*) /\$1 break;
-        proxy_pass http://grafana/;
+        rewrite ^/grafana/(.*) /\$1 break;
+        proxy_pass http://grafana;
         proxy_set_header Host \$http_host;
         proxy_set_header X-Real-IP \$remote_addr;
         proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
@@ -150,8 +150,8 @@ server {
 
     # Proxy Grafana Live WebSocket connections
     location /grafana/api/live/ {
-        rewrite  ^/grafana/(.*) /\$1 break;
-        proxy_pass http://grafana/;
+        rewrite ^/grafana/(.*) /\$1 break;
+        proxy_pass http://grafana;
         proxy_http_version 1.1;
         proxy_set_header Upgrade \$http_upgrade;
         proxy_set_header Connection \$connection_upgrade;
