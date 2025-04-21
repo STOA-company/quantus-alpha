0d8f84bfa230295e538ce0ec0e3d27e5bff54960
kyungmin-stoa <rhrudals312@stoa-investment.com>
2025-04-21 18:22:56 +0900

    fix : 게시글 생성 1차

--- app/api/v2.py
+++ app/api/v2.py
@@ -1,5 +1,7 @@
 from fastapi import APIRouter
 
+from app.modules.community.v2.router import router as community_router
+
 api_router = APIRouter()
 
-# api_router.include_router(price_router, prefix="/price", tags=["price"])
+api_router.include_router(community_router, prefix="/community", tags=["community/v2"])
