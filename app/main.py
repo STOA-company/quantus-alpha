84db0f5000b4ca6d5e6bd2eb24f7be93a2279da9
kyungmin-stoa <rhrudals312@stoa-investment.com>
2025-04-22 17:06:33 +0900

    fix : cors 허용 (로컬 3001)

--- app/main.py
+++ app/main.py
@@ -73,6 +73,8 @@ def root():
 origins = [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
+    "http://localhost:3001",
+    "http://127.0.0.1:3001",
     "http://localhost:8000",
     "http://127.0.0.1:8000",
     "https://alpha-dev.quantus.kr",
