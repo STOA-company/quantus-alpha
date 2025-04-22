8c71708ae068739487e21610a0c827adedf62a84
kyungmin-stoa <rhrudals312@stoa-investment.com>
2025-04-22 20:28:17 +0900

    Feat/m news  (#722)
    * feat : 유저 db 변경 api (뉴스)
    
    * feat : [app][상세] 공시 api 수정
    
    * fix : parquet 파일 컬럼명 수정

--- app/api/v2.py
+++ app/api/v2.py
@@ -1,7 +1,11 @@
 from fastapi import APIRouter
 
 from app.modules.community.v2.router import router as community_router
+from app.modules.disclosure.v2.router import router as disclosure_router
+from app.modules.news.v2.router import router as news_router
 
 api_router = APIRouter()
 
 api_router.include_router(community_router, prefix="/community", tags=["community/v2"])
+api_router.include_router(news_router, prefix="/news", tags=["news/v2"])
+api_router.include_router(disclosure_router, prefix="/disclosure", tags=["disclosure/v2"])
