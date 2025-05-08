2155d969a1a6bbb6c653e71ddbe97d40b583d407
kyungmin-stoa <rhrudals312@stoa-investment.com>
2025-05-08 16:26:28 +0900

    fix : mock res 수정 ( 팔로우, 팔로잉 )

--- app/modules/community/v2/mock_data.py
+++ app/modules/community/v2/mock_data.py
@@ -1,65 +1,71 @@
-followers = [
-    {
-        "id": 1,
-        "nickname": "John Doe",
-        "profile_image": "default_image/Alpaca.png",
-        "image_format": "png",
-        "is_official": True,
-        "is_followed": True,
-    },
-    {
-        "id": 2,
-        "nickname": "Jane Smith",
-        "profile_image": "default_image/Frog.png",
-        "image_format": "png",
-        "is_official": True,
-        "is_followed": False,
-    },
-    {
-        "id": 3,
-        "nickname": "Alice Johnson",
-        "profile_image": "default_image/Vulture.png",
-        "image_format": "png",
-        "is_official": False,
-        "is_followed": True,
-    },
-    {
-        "id": 4,
-        "nickname": "Bob Brown",
-        "profile_image": "default_image/Cat.png",
-        "image_format": "png",
-        "is_official": False,
-        "is_followed": False,
-    },
-]
+followers = {
+    "followers_count": 4,
+    "followers": [
+        {
+            "id": 1,
+            "nickname": "John Doe",
+            "profile_image": "default_image/Alpaca.png",
+            "image_format": "png",
+            "is_official": True,
+            "is_followed": True,
+        },
+        {
+            "id": 2,
+            "nickname": "Jane Smith",
+            "profile_image": "default_image/Frog.png",
+            "image_format": "png",
+            "is_official": True,
+            "is_followed": False,
+        },
+        {
+            "id": 3,
+            "nickname": "Alice Johnson",
+            "profile_image": "default_image/Vulture.png",
+            "image_format": "png",
+            "is_official": False,
+            "is_followed": True,
+        },
+        {
+            "id": 4,
+            "nickname": "Bob Brown",
+            "profile_image": "default_image/Cat.png",
+            "image_format": "png",
+            "is_official": False,
+            "is_followed": False,
+        },
+    ],
+}
 
-following = [
-    {
-        "id": 1,
-        "nickname": "John Doe",
-        "profile_image": "default_image/Pig.png",
-        "image_format": "png",
-        "is_official": False,
-    },
-    {
-        "id": 2,
-        "nickname": "Jane Smith",
-        "profile_image": "default_image/Lizard.png",
-        "image_format": "png",
-        "is_official": True,
-    },
-    {
-        "id": 3,
-        "nickname": "Alice Johnson",
-        "profile_image": "default_image/Hippopotamus.png",
-        "image_format": "png",
-        "is_official": False,
-    },
-    {
-        "id": 4,
-        "nickname": "Bob Brown",
-        "profile_image": "default_image/Otter.png",
-        "image_format": "png",
-        "is_official": True,
-    },
-]
+following = {
+    "following_count": 4,
+    "following": [
+        {
+            "id": 1,
+            "nickname": "John Doe",
+            "profile_image": "default_image/Pig.png",
+            "image_format": "png",
+            "is_official": False,
+        },
+        {
+            "id": 2,
+            "nickname": "Jane Smith",
+            "profile_image": "default_image/Lizard.png",
+            "image_format": "png",
+            "is_official": True,
+        },
+        {
+            "id": 3,
+            "nickname": "Alice Johnson",
+            "profile_image": "default_image/Hippopotamus.png",
+            "image_format": "png",
+            "is_official": False,
+        },
+        {
+            "id": 4,
+            "nickname": "Bob Brown",
+            "profile_image": "default_image/Otter.png",
+            "image_format": "png",
+            "is_official": True,
+        },
+    ],
+}
