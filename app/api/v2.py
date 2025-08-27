7e99fa1e9ccd7cdc525405d3214d7534a13e11d4
kknakss <kknaks@stoa-investment.com>
2025-08-27 11:26:59 +0900

    merge: to dev

--- app/api/v2.py
+++ app/api/v2.py
@@ -5,6 +5,7 @@ from app.modules.disclosure.v2.router import router as disclosure_router
 from app.modules.interest.v2.router import router as interest_router
 from app.modules.news.v2.router import router as news_router
 from app.modules.user.v2.router import router as user_router
+from app.modules.load_test.router import router as load_test_router
 
 api_router = APIRouter()
 
@@ -13,3 +14,4 @@ api_router.include_router(news_router, prefix="/news", tags=["news/v2"])
 api_router.include_router(disclosure_router, prefix="/disclosure", tags=["disclosure/v2"])
 api_router.include_router(interest_router, prefix="/interest", tags=["interest/v2"])
 api_router.include_router(user_router, prefix="/user", tags=["user/v2"])
+api_router.include_router(load_test_router, prefix="/load-test", tags=["load-test"])
